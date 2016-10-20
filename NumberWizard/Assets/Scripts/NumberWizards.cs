991661242d264a7d2954b5c0c0434e3100d3def7
ToastyMod <toastymod@gmail.com>
2016-10-20 17:23:02 -0600

    inital upload

---
+++ NumberWizard/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberWizards : MonoBehaviour {
+
+	int max = 1000;
+	int min = 1;
+
+	// Use this for initialization
+	void Start () {
+		print("==Number Wizard==");
+		print("Pick a random number");
+		print("The highest number you can pick is " + max);
+		print("The lowset number you can pick is " + min);
+
+	}
+
+	void StartGame(){
+		
+	}
+
+	// Update is called once per frame
+	void Update () {
+	if(Input.GetKeyDown(KeyCode.DownArrow)){
+		print("down arrow pressed");
+	}
+	if(Input.GetKeyDown(KeyCode.UpArrow)){
+		print("up arrow pressed");
+	}
+	}
+}
---
+++ Text101/Assets/TextController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+
+	public Text mytext;
+	public int mindex = 0;
+	public string[] menu = {"start game","quit"};
+	public enum lollol {asdf,ghgj,fgh};
+	public lollol states;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	void onKeyPressDo(float kc int func){
+		if(Input.GetKeyDown(kc)){
+			int lol;
+			lol
+			func();
+		}
+	}
+
+	// Update is called once per frame
+
+	void Update ()
+	{
+		onKeyPressDo(KeyCode.S, new int(){print("ay it works lmao")})
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			mindex = 0;
+			mytext.text = menu [mindex];
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			mindex = 1;
+			mytext.text = menu [mindex];
+		}
+	}
+}
